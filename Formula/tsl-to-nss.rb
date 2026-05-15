e4b24ca1b78eb8cf852c3f9664acc54f3105f9f8
Antonio Vera <2104277+anvera@users.noreply.github.com>
2026-05-14 22:12:20 -0700

    Set sha256 for v0.1.0 release

--- Formula/tsl-to-nss.rb
+++ Formula/tsl-to-nss.rb
@@ -2,7 +2,7 @@ class TslToNss < Formula
   desc "Import TSL certificates into an NSS database via certutil"
   homepage "https://github.com/anvera/homebrew-tsl-to-nss"
   url "https://github.com/anvera/homebrew-tsl-to-nss/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "f559c965dca1f210b1d4b0a2c33be904ab40be58b6006db2b22ce57b8f72160a"
   license "MIT"
 
   depends_on "anvera/tsl-extract/tsl-extract"
