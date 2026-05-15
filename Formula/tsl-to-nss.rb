6dd792133ac793e16c568087ebb4e170d5bf255f
Antonio Vera <2104277+anvera@users.noreply.github.com>
2026-05-14 22:47:38 -0700

    Bump formula to v0.2.0

--- Formula/tsl-to-nss.rb
+++ Formula/tsl-to-nss.rb
@@ -1,8 +1,8 @@
 class TslToNss < Formula
   desc "Import TSL certificates into an NSS database via certutil"
   homepage "https://github.com/anvera/homebrew-tsl-to-nss"
-  url "https://github.com/anvera/homebrew-tsl-to-nss/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f559c965dca1f210b1d4b0a2c33be904ab40be58b6006db2b22ce57b8f72160a"
+  url "https://github.com/anvera/homebrew-tsl-to-nss/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "7b092e213d6614834d0553012e7a1455c6d014b03db409c98d66f479b9cccaa1"
   license "MIT"
 
   depends_on "anvera/tsl-extract/tsl-extract"
